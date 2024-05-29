f92e2c97756d8c6d55bd7bf752124ab0a828899e
Ekaterina <108530800+piccolo-gatto@users.noreply.github.com>
2024-05-29 22:54:29 +0800

    Fix feed function for click

--- PetFeeder.ino
+++ PetFeeder.ino
@@ -1,25 +1,16 @@
-#include <ezButton.h>
-
-ezButton button(4);
-
-
+#include "lib/GyverStepper.h"
+#include "lib/EncButton.h"
 #include <EEPROM.h>
 
-#include <CustomStepper.h>            // Подключаем библиотеку CustomStepper
-CustomStepper stepper(13, 12, 11, 10);  // Указываем пины, к которым подключен драйвер шагового двигателя
-//#define STEPS_FRW 2        // шаги вперёд
-//#define STEPS_BKW 1       // шаги назад
+#define EE_RESET 12
+#define BTN_PIN 4
+#define LED_PIN 7	
 
-#define BTN_PIN 4           // кнопка
-#define LED_PIN 7
-#include "EncButton.h"
+
+GStepper<STEPPER4WIRE> stepper(2048, 10, 12, 11, 13);
 EncButton<EB_TICK, BTN_PIN> btn;
 
-int feedAmount = 100;
-
-
-
-boolean rotate_dir = false;
+int feedAmount = 3000;
 
 
 void setup()
@@ -28,78 +19,49 @@ void setup()
 
   pinMode(LED_PIN, OUTPUT);
 
-  Serial.begin(115200);
-  stepper.setRPM(17);                 // Устанавливаем количество оборотов в минуту
-  stepper.setSPR(4000);       // Устанавливаем колочество шагов на полный оборот. Максимальное значение 4075.7728395
+  Serial.begin(9600);
+
+  stepper.setMaxSpeed(3000);
+  stepper.setAcceleration(500);
+
 }
 
 void loop()
 {
-  button.loop();
 
+  btn.tick();
 
-  //btn.tick();
-  // if (stepper.isDone() &&  btn.click())  
-  // {
-  //   stepper.setDirection(CCW);        // Устанавливает направление вращения (по часовой)
-  //   stepper.rotate(2);                // Устанавливает вращение на заданное количество оборотов
-  //   //rotate1 = true;
-  // }
-  // if (stepper.isDone() && rotate1 == true && rotatedeg == false)
-  // {
-  //   stepper.setDirection(CW);         // Устанавливает направление вращения (против часовой)
-  //   stepper.rotateDegrees(90);        // Поворачивает вал на заданное кол-во градусов
-  //   rotatedeg = true;
-  // }
+  if (btn.click()){
+    digitalWrite(LED_PIN, HIGH);
+    feed();
+    digitalWrite(LED_PIN, LOW);
+  }
 
-  // if (btn.click()){
-  //   digitalWrite(LED_PIN, HIGH);
-  //   feed();
-  //   digitalWrite(LED_PIN, LOW);
-  // }    
-
-  // if (btn.hold()) {
-  //   digitalWrite(LED_PIN, HIGH);
-  //   int newAmount = 0;
-  //   while (btn.isHold()) {
-  //     btn.tick();
-  //     anti_jam();
-  //     newAmount++;
-  //   }
-  //   stepper.setDirection(STOP);
-  //   feedAmount = newAmount;
-  //   EEPROM.put(1, feedAmount);
-  //   digitalWrite(LED_PIN, LOW);
-  // }
-
-  if(button.isPressed()){
-    //Serial.println("The button is pressed");
-    feed();    
-  }       
-  //anti_jam();    
+  if (btn.hold()) {
+    digitalWrite(LED_PIN, HIGH);
+    int newAmount = 0;
+    while (btn.isHold()) {
+      btn.tick();
+      anti_jam();
+      newAmount++;
+    }
+    feedAmount = newAmount;
+    EEPROM.put(1, feedAmount);
+    digitalWrite(LED_PIN, LOW);
+  }
 }
 
 void feed() {
-  for (int i=0; i< 5000; i++){
+  for (int i=0; i< feedAmount; i++){
+    Serial.println(i);
     anti_jam();
   }
 }
 
 void anti_jam() {
-  if (stepper.isDone() && rotate_dir == false){
-    stepper.setDirection(CCW); 
-    //stepper.rotate(2);
-    //stepper.rotate();
-    stepper.rotateDegrees(25);
-    rotate_dir = true;
+  if (!stepper.tick()) {
+    static bool dir;
+    dir = !dir;
+    stepper.setTargetDeg(dir ? -1: 70);
   }
-  if (stepper.isDone() && rotate_dir == true){
-    stepper.setDirection(CW);   
-    //stepper.rotate(1);
-    stepper.rotateDegrees(70);
-    //stepper.setDirection(STOP);
-    rotate_dir = false;
-  }
-  stepper.run();
 }
-
